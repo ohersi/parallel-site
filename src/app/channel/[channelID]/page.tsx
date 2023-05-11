3ad0d5b4c9e305ddfb3d29b3ffdd43b132a72ed2
Omar Hersi <o.hersi@yahoo.com>
2023-05-11 19:55:27 -0400

    - Create dynamic route for channel page, fetching channel data, set metadata - Create types file, add Channel and Block types

---
+++ src/app/channel/[channelID]/page.tsx
@@ -0,0 +1,44 @@
+// Packages
+import axios from 'axios';
+import { Metadata } from 'next';
+// Imports
+import { Channel } from '@/app/types';
+
+interface PageProps {
+    params: {
+        channelID: string
+    }
+}
+
+// TODO: Use state management to fetch data and populate pages (Redux?)
+async function getChannelData(props: PageProps) {
+    const res = await axios.get(`http://localhost:3000/api/v1/channels/${props.params.channelID}`);
+    console.log('data fetched');
+    // Contains Page info and channel data
+    const data = await res.data;
+    return data;
+}
+
+// Dynamic Metadata for Pages
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+
+    const data = await getChannelData(props);
+    const channel: Channel = data.data;
+
+    return { title: `${channel.title} — Parallel` };
+}
+
+const page = async (props: PageProps) => {
+
+    // database fetching
+    const post = await getChannelData(props);
+
+    return (
+        <div>
+            {JSON.stringify(post)}
+        </div>
+
+    )
+};
+
+export default page;
