ffb8d75633a588a6745eefbe119ae2c0843617ce
Omar Hersi <o.hersi@yahoo.com>
2023-07-29 20:58:44 -0400

    Create styling for home page (WIP)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Nav from '@/components/navigation/nav';
 import NavLogo from '@/components/navigation/logo.navigation';
 import NavProfile from '@/components/navigation/profile.navigation';
 import NavSearch from '@/components/navigation/search.navigation';
+import styles from '@/styles/pages/home.page.module.scss';
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -21,7 +22,23 @@ const HomePage = async () => {
   return (
     <main>
       <Nav search={<NavSearch />} logo={<NavLogo />} profile={<NavProfile />} />
-      <div>HOME PAGE</div>
+
+      <div className={styles.page}>
+        <div className={styles.page__logo}>
+          <div className={styles.page__logo__title}>
+            PARA <span className={styles.page__logo__title__font}>/</span> LLEL
+          </div>
+        </div>
+        <div className={styles.page__logo__svg}>
+          <svg
+            className={styles.page__logo__svg__test}
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 250" fill="none">
+            <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 92.4198)" fill="#FCF9F8" stroke="#215BDF" />
+            <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 46.3097)" fill="#FCF9F8" stroke="#215BDF" />
+            <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 0.199837)" fill="#FCF9F8" stroke="#215BDF" />
+          </svg>
+        </div>
+      </div>
       <div>
         {
           cookieStore.getAll().map((cookie) => (
