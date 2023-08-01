e07960a931a388ccb2b8d46975fdb358f21c5a1d
Omar Hersi <o.hersi@yahoo.com>
2023-08-01 00:33:22 -0400

    Update home page styling hover for desktop and mobile, and update nav profile menu icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 // Packages
-import { cookies } from 'next/headers';
 import { Metadata } from 'next';
+import Link from 'next/link';
 // Imports
-import DefaultFeed from '@/components/feed/default.feed';
 import Nav from '@/components/navigation/nav';
 import NavLogo from '@/components/navigation/logo.navigation';
 import NavProfile from '@/components/navigation/profile.navigation';
@@ -16,38 +15,36 @@ export const metadata: Metadata = {
 
 const HomePage = async () => {
 
-  const cookieStore = cookies();
-  const session = cookieStore.has(process.env.SESSION_ID!);
-
   return (
     <main>
       <Nav search={<NavSearch />} logo={<NavLogo />} profile={<NavProfile />} />
 
       <div className={styles.page}>
-        <div className={styles.page__logo}>
-          <div className={styles.page__logo__title}>
-            PARA <span className={styles.page__logo__title__font}>/</span> LLEL
-          </div>
-        </div>
-        <div className={styles.page__logo__svg}>
-          <svg
-            className={styles.page__logo__svg__test}
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 250" fill="none">
-            <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 92.4198)" fill="#FCF9F8" stroke="#215BDF" />
-            <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 46.3097)" fill="#FCF9F8" stroke="#215BDF" />
-            <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 0.199837)" fill="#FCF9F8" stroke="#215BDF" />
-          </svg>
-        </div>
-      </div>
-      <div>
-        {
-          cookieStore.getAll().map((cookie) => (
-            <div key={cookie.name}>
-              <p>Name: {cookie.name}</p>
-              <p>Value: {cookie.value}</p>
+
+        <div className={styles.page__main}>
+
+          <div className={styles.page__main__logo}>
+            <div className={styles.page__main__logo__title}>
+              PARA <span className={styles.page__main__logo__title__font}>/</span> LLEL
             </div>
-          ))
-        }
+          </div>
+
+          <div className={styles.page__main__svg_container}>
+            <svg
+              className={styles.page__main__svg_container__svg}
+              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 250" fill="none">
+              <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 92.4198)" fill="#FCF9F8" stroke="#215BDF" />
+              <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 46.3097)" fill="#FCF9F8" stroke="#215BDF" />
+              <rect x="0.047914" y="0.375849" width="207.645" height="207.645" transform="matrix(0.965926 0.258819 -0.870098 0.492879 181.937 0.199837)" fill="#FCF9F8" stroke="#215BDF" />
+            </svg>
+          </div>
+
+          <div className={styles.page__main__link}>
+            <Link href={`/explore`}>explore &#8594;</Link>
+          </div>
+
+        </div>
+
       </div>
     </main>
   )
--- src/components/navigation/profile.navigation.tsx
+++ src/components/navigation/profile.navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 // Packages
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { usePathname } from 'next/navigation'
 import Link from "next/link";
 // Imports
@@ -18,7 +18,7 @@ const NavProfile = () => {
     const isMenuOpen = useAppSelector((state) => state.Menu.isMenuOpen);
 
     useEffect(() => {
-      dispatch(setIsMenuOpen(false));
+        dispatch(setIsMenuOpen(false));
     }, [pathname]);
 
     return (
@@ -34,8 +34,9 @@ const NavProfile = () => {
 
                         <div className={isMenuOpen ? styles.nav__menu__dropdown : styles.nav__menu__dropdown__hidden}>
 
-                            <div className={styles.nav__menu__dropdown__close_btn}>
+                            <div className={styles.nav__menu__dropdown__close}>
                                 <svg
+                                    className={styles.nav__menu__dropdown__close__btn}
                                     onClick={() => dispatch(setIsMenuOpen(!isMenuOpen))}
                                     xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 15" fill="none">
                                     <line x1="1.96612" y1="0.427971" x2="16.0827" y2="14.5446" stroke="currentColor" />
@@ -79,7 +80,7 @@ const NavProfile = () => {
                     </div>
                     :
                     <div className={styles.nav__account}>
-                        <div className={styles.nav__account__desktop}>
+                        {/* <div className={styles.nav__account__desktop}>
                             <Link href={'/login'}>
                                 <button className={styles.nav__account__desktop__login}>
                                     login
@@ -90,22 +91,28 @@ const NavProfile = () => {
                                     sign up
                                 </button>
                             </Link>
-                        </div>
+                        </div> */}
                         <div className={styles.nav__account__mobile}>
-                            <div
-                                className={styles.nav__account__mobile__btn}>
-                                <svg
+                            <div className={styles.nav__account__mobile__btn}>
+                                {/* <svg
                                     onClick={() => dispatch(setIsMenuOpen(!isMenuOpen))}
                                     xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
                                     <path d="M17.305 6.02387C17.305 8.67243 15.1579 10.8195 12.5093 10.8195C9.86077 10.8195 7.71368 8.67243 7.71368 6.02387C7.71368 3.3753 9.86077 1.22821 12.5093 1.22821C15.1579 1.22821 17.305 3.3753 17.305 6.02387Z" stroke="currentColor" />
                                     <path d="M24.1995 25.2679C24.1995 23.7327 23.8971 22.2126 23.3096 20.7943C22.7221 19.376 21.861 18.0873 20.7755 17.0017C19.69 15.9162 18.4013 15.0551 16.9829 14.4676C15.5646 13.8801 14.0445 13.5778 12.5093 13.5778C10.9741 13.5778 9.45399 13.8801 8.03568 14.4676C6.61736 15.0551 5.32865 15.9162 4.24312 17.0017C3.15759 18.0873 2.2965 19.376 1.70901 20.7943C1.12153 22.2126 0.819153 23.7327 0.819153 25.2679" stroke="currentColor" />
+                                </svg> */}
+                                <svg
+                                    onClick={() => dispatch(setIsMenuOpen(!isMenuOpen))}
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="16" viewBox="0 0 24 16" fill="none">
+                                    <line x1="0.190598" y1="14.71" x2="23.5851" y2="14.71" stroke="#215BDF" />
+                                    <line x1="0.190598" y1="0.709961" x2="23.5851" y2="0.709961" stroke="#215BDF" />
                                 </svg>
                             </div>
 
                             <div className={isMenuOpen ? styles.nav__menu__dropdown : styles.nav__menu__dropdown__hidden}>
 
-                                <div className={styles.nav__menu__dropdown__close_btn}>
+                                <div className={styles.nav__menu__dropdown__close}>
                                     <svg
+                                        className={styles.nav__menu__dropdown__close__btn}
                                         onClick={() => dispatch(setIsMenuOpen(!isMenuOpen))}
                                         xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 15" fill="none">
                                         <line x1="1.96612" y1="0.427971" x2="16.0827" y2="14.5446" stroke="currentColor" />
