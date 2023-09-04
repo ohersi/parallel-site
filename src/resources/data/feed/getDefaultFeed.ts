a2f351dbdb86961805c59af11625764192a3dfb1
Omar Hersi <o.hersi@yahoo.com>
2023-09-04 01:07:00 -0400

    Change from next revalidate to cache 'no-store' in getDefaultFeed api call function

--- src/resources/data/feed/getDefaultFeed.ts
+++ src/resources/data/feed/getDefaultFeed.ts
@@ -26,7 +26,7 @@ export async function GetDefaultFeed(channel_lastID?: string | null | undefined,
                 "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
                 "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
             },
-            next: { revalidate: 0 },
+            cache: 'no-store',
         });
 
         if (res.status === 404) {
